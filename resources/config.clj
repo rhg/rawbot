9ad67c7a40faf8bf78dff1aa58516e062ac55239
Ricardo Gomez <ricardo.gomez331@gmail.com>
2013-08-18 19:02:34 -0500

    add aa51 irc

--- resources/config.clj
+++ resources/config.clj
@@ -1,8 +1,16 @@
 {:master "rhg135"
  :servers
-  {"irc.freenode.net"
-    {:nick "rawbot-test"
+   {"quebec.androidarea51.com"
+     {:nick "rawbot"
+      :channels ["#rawbot" "#staffhome"]
+      :prefix "~"
+      :password nil
+      :port 6667
+      :ssl? false}
+  "irc.freenode.net"
+    {:nick "rawbot-clj"
      :channels ["#rawbot-clj"]
+     :prefix "$"
      :password nil
      :port 6667
      :ssl? false}}}
